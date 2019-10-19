e32f86a7befcfd00ac72c56d84a4441cb63e2afe
Berk Altıok <berkpw@gmail.com>
2019-10-19 14:26:30 +0300

    update meta

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,13 +14,17 @@ const app = new Vue({
   router,
   data() {
     return {
-      config: {},
+      config: null,
       year: new Date().getFullYear()
     }
   },
   beforeCreate() {
     axios.get('config.json').then(r => {
       this.config = JSON.parse(JSON.stringify(r.data));
+    }).then(() => {
+      document.title = this.config.site.title;
+      $('meta[name="description"]').attr("content", this.config.site.desc);
+      $('link[rel="shortcut icon"]').attr("href", this.config.site.favicon);
     });
   }
 }).$mount('#app');
