a7774e36a2220e680daa8eaa2105ca2fbe0f08da
Berk Altıok <berkpw@gmail.com>
2019-10-21 00:02:29 +0300

    update config system

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,18 +18,19 @@ const app = new Vue({
       config: ""
     }
   },
+  methods: {
+    openItem: function(item, index) {
+      console.log(item, index)
+    }
+  },
   beforeCreate() {
-    let isCustom = false;
-    axios.get('custom-config.json').then(e => {
-      if (e.data.site) isCustom = true;
-      axios.get(isCustom ? 'custom-config.json' : 'config.json').then(r => {
-        this.config = JSON.parse(JSON.stringify(r.data));
-      }).then(() => {
-        document.title = this.config.site.title;
-        $('meta[name="description"]').attr("content", this.config.site.desc);
-        $('link[rel="shortcut icon"]').attr("href", this.config.site.favicon);
-        this.load = true;
-      });
+    axios.get('config.json').then(r => {
+      this.config = JSON.parse(JSON.stringify(r.data));
+    }).then(() => {
+      document.title = this.config.site.title;
+      $('meta[name="description"]').attr("content", this.config.site.desc);
+      $('link[rel="shortcut icon"]').attr("href", this.config.site.favicon);
+      this.load = true;
     });
   }
 }).$mount('#app');
