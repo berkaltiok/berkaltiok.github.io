8e45663a5dca717fc031b7b7a9a91f6fadaa8f07
Berk Altıok <berkpw@gmail.com>
2019-10-20 23:56:36 +0300

    add portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,13 +19,17 @@ const app = new Vue({
     }
   },
   beforeCreate() {
-    axios.get('config.json').then(r => {
-      this.config = JSON.parse(JSON.stringify(r.data));
-    }).then(() => {
-      document.title = this.config.site.title;
-      $('meta[name="description"]').attr("content", this.config.site.desc);
-      $('link[rel="shortcut icon"]').attr("href", this.config.site.favicon);
-      this.load = true;
+    let isCustom = false;
+    axios.get('custom-config.json').then(e => {
+      if (e.data.site) isCustom = true;
+      axios.get(isCustom ? 'custom-config.json' : 'config.json').then(r => {
+        this.config = JSON.parse(JSON.stringify(r.data));
+      }).then(() => {
+        document.title = this.config.site.title;
+        $('meta[name="description"]').attr("content", this.config.site.desc);
+        $('link[rel="shortcut icon"]').attr("href", this.config.site.favicon);
+        this.load = true;
+      });
     });
   }
 }).$mount('#app');
